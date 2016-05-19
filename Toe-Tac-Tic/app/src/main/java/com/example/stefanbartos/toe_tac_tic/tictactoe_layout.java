5660114daa7e83808eb81c64a207986e34ce7212
stibo123 <stibo123@gmail.com>
2016-05-19 15:48:22 +0200

    Tic-Tac-Toe Layout
    Added Tic-Tac-Toe Layout

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/tictactoe_layout.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/tictactoe_layout.java
@@ -1,24 +1,18 @@
 package com.example.stefanbartos.toe_tac_tic;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class tictactoe_layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tictactoe_layout);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
 
-    public void oneplayer()
-    {
-        Intent intent = new Intent(this, tictactoe_layout.class);
-        startActivity(intent);
-    }
+
 }
