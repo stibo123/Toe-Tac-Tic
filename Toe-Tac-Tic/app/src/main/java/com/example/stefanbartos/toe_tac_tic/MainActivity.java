f61e84b528eaeaa5e837417b90f6f3ee6b1a0216
diepes7 <olivero.dieplinger@gmail.com>
2016-06-09 13:47:57 +0200

    MainActivity
    OnClickListener for every Button
    Intents

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/MainActivity.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/MainActivity.java
@@ -5,8 +5,15 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonOnePlayer;
+    private Button buttonTwoPlayer;
+    private Button buttonBTGame;
+    private Button buttonStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +21,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        initButtons();
+
     }
 
-    public void oneplayer()
-    {
-        Intent intent = new Intent(this, tictactoe_layout.class);
-        startActivity(intent);
+    public void initButtons(){
+        buttonOnePlayer = (Button) findViewById(R.id.buttonOnePlayer);
+        buttonTwoPlayer = (Button) findViewById(R.id.buttonTwoPlayer);
+        buttonBTGame = (Button) findViewById(R.id.buttonBTGame);
+        buttonStats = (Button) findViewById(R.id.buttonStats);
+
+        buttonOnePlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent OPintent = new Intent(String.valueOf(OnePlayer.class));
+                startActivity(OPintent);
+            }
+        });
+
+        buttonTwoPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent TPintent = new Intent(TwoPlayer.class);
+                startActivity(TPintent);
+            }
+        });
+
+        buttonBTGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent BTGame = new Intent(BTGame.class);
+                startActivity(BTGame);
+            }
+        });
+
+        buttonStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent statsIntent = new Intent(Stats.class);
+                startActivity(statsIntent);
+            }
+        });
     }
+
+
 }
---
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/OnePlayer.java
@@ -0,0 +1,7 @@
+package com.example.stefanbartos.toe_tac_tic;
+
+/**
+ * Created by odieplinger on 09.06.2016.
+ */
+public class OnePlayer {
+}
