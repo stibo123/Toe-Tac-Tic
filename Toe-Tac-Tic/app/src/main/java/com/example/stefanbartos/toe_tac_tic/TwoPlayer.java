8f2344e1a8fcb4166eb1c085c9704454200cd843
diepes7 <olivero.dieplinger@gmail.com>
2016-06-09 15:55:02 +0200

    TwoPlayer
    Erweiterung

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/TwoPlayer.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/TwoPlayer.java
@@ -2,6 +2,7 @@ package com.example.stefanbartos.toe_tac_tic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class TwoPlayer extends AppCompatActivity {
@@ -16,12 +17,15 @@ public class TwoPlayer extends AppCompatActivity {
     private Button bot2;
     private Button bot3;
 
+    private String sign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tictactoe_layout);
 
         initField();
+        selection();
     }
 
     public void initField() {
@@ -33,6 +37,79 @@ public class TwoPlayer extends AppCompatActivity {
         mid3 = (Button) findViewById(R.id.midright);
         bot1 = (Button) findViewById(R.id.bottomleft);
         bot2 = (Button) findViewById(R.id.bottom);
-       // bot3 = (Button) findViewById(R.id.)
+        bot3 = (Button) findViewById(R.id.bottomright);
+    }
+
+    public void selection(){
+        top1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top1.setText();
+            }
+        });
+
+        top2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top2.setText("X");
+            }
+        });
+
+        top3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top3.setText(playerTurn());
+            }
+        });
+
+        mid1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mid1.setText("X");
+            }
+        });
+
+        mid2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mid2.setText("X");
+            }
+        });
+
+        mid3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mid3.setText("X");
+            }
+        });
+
+        bot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bot1.setText("X");
+            }
+        });
+
+        bot2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bot2.setText("X");
+            }
+        });
+
+        bot3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bot3.setText("X");
+            }
+        });
+    }
+
+    public String playerTurn(){
+        int cnt = 0;
+        if (cnt % 2){
+            sign = "X";
+        }
+        return sign;
     }
 }
