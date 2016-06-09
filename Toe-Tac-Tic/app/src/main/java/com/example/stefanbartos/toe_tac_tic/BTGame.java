9916aaffe69ddb22e4c0c5a4c44f49297d30d331
diepes7 <olivero.dieplinger@gmail.com>
2016-06-09 14:56:25 +0200

    Bluetooth Class
    First step to Connect via Bluetooth

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/BTGame.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/BTGame.java
@@ -1,7 +1,62 @@
 package com.example.stefanbartos.toe_tac_tic;
 
-/**
- * Created by odieplinger on 09.06.2016.
- */
-public class BTGame {
-}
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+public class BTGame extends AppCompatActivity {
+
+    private ArrayAdapter mArrayAdapter;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btgame);
+        initBluetoothAdapter();
+    }
+
+    public void initBluetoothAdapter(){
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(), "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            }
+        if (!mBluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+    // Register the BroadcastReceiver
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+    //registerReceiver(mReceiver, filter) {
+
+    }
+
+
+
--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/MainActivity.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         buttonBTGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Bluetooth eingeschaltet? ;)", Toast.LENGTH_LONG).show();
                 Intent BTGame = new Intent(getApplicationContext(), BTGame.class);
                 startActivity(BTGame);
             }
