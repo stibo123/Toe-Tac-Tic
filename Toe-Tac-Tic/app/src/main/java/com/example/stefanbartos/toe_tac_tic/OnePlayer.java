c0960f00b480533a4ed73b04830607336c426624
stibo123 <stibo123@gmail.com>
2016-06-09 15:35:21 +0200

    Changed Variable

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/OnePlayer.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/OnePlayer.java
@@ -1,8 +1,42 @@
 package com.example.stefanbartos.toe_tac_tic;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
 /**
  * Created by odieplinger on 09.06.2016.
  */
-public class OnePlayer {
+public class OnePlayer extends AppCompatActivity{
+    Button topleft;
+    Button top;
+    Button topright;
+    Button midright;
+    Button mid;
+    Button midleft;
+    Button bottomleft;
+    Button bottom;
+    Button bottomright;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tictactoe_layout);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        initVariables();
+    }
+
+    private void initVariables() {
+        topleft = (Button) findViewById(R.id.topleft);
+        top = (Button) findViewById(R.id.top);
+        topright = (Button) findViewById(R.id.topright);
+        midleft = (Button) findViewById(R.id.midleft);
+        mid = (Button) findViewById(R.id.mid);
+        midright = (Button) findViewById(R.id.midright);
+        bottomleft = (Button) findViewById(R.id.bottomleft);
+        bottom = (Button) findViewById(R.id.bottom);
+        bottomright = (Button) findViewById(R.id.bottomright);
+    }
 
 }
