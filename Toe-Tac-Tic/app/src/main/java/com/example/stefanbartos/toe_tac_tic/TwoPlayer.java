0c8c6f64ef2677460ccdf0bbcce8c9dc145d0bfa
diepes7 <olivero.dieplinger@gmail.com>
2016-06-16 15:39:43 +0200

    TwoPlayer
    findWinner

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/TwoPlayer.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/TwoPlayer.java
@@ -25,23 +25,32 @@ public class TwoPlayer extends AppCompatActivity {
     private Button bot2;
     private Button bot3;
 
-    private TextView player1;
+    private TextView player1Name;
+    private TextView player2Name;
+    private TextView points_p1;
+    private TextView points_p2;
+
+    private Character[][] fieldArray;
 
     private int clickCNT = 0;
     private int cnt = 1;
-    private String winner; //dfadffmfdk
+    private String winner;
+
+    String p1_name;
+    String p2_name;
 
     private String sign;
 
-    private Player player;
+    private Player playerObject1;
+    private Player playerObject2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tictactoe_layout);
 
-        playerInput();
         initField();
+        playerInput();
         selection();
     }
 
@@ -55,12 +64,22 @@ public class TwoPlayer extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 EditText e1 = (EditText) vDialog.findViewById(R.id.editTextP1Name);
                 EditText e2 = (EditText) vDialog.findViewById(R.id.editTextP2Name);
-                player = new Player(e1.getText().toString(), 0);
-                player = new Player(e2.getText().toString(), 0);                                    //unn�tiges anlegen von Player, m�ssen bei Punkteberechung angelegt werden wegen Felder Name und Punkte
+
+                p1_name = e1.getText() + "";
+                p2_name = e2.getText() + "";
+
+                playerObject1 = new Player(e1.getText().toString(), 0);
+                playerObject2 = new Player(e2.getText().toString(), 0);                                    //unn�tiges anlegen von Player, m�ssen bei Punkteberechung angelegt werden wegen Felder Name und Punkte
             }
         });
-
+        alert.setNegativeButton("CANCEL", null);
         alert.show();
+
+        playerObject1 = new Player(p1_name, 0);
+        playerObject2 = new Player(p2_name, 0);
+
+        player2Name.setText(p1_name);
+        player2Name.setText(p2_name);
     }
 
 
@@ -74,6 +93,14 @@ public class TwoPlayer extends AppCompatActivity {
         bot1 = (Button) findViewById(R.id.bottomleft);
         bot2 = (Button) findViewById(R.id.bottom);
         bot3 = (Button) findViewById(R.id.bottomright);
+
+        fieldArray = new Character[2][2];
+
+        player1Name = (TextView) findViewById(R.id.ainametv);
+        player2Name = (TextView) findViewById(R.id.playernametv);
+
+        points_p1 = (TextView) findViewById(R.id.aigameswontv);
+        points_p2 = (TextView) findViewById(R.id.playergameswontv);
     }
 
     public void selection() {
@@ -150,19 +177,191 @@ public class TwoPlayer extends AppCompatActivity {
         });
     }
 
-    public String playerTurn(Button b) {
-        if (clickCNT <= 9) {
+    public String playerTurn(Button button) {
+        if (clickCNT < 9) {
             if (cnt % 2 == 0) {
-                b.setBackgroundResource(R.drawable.x);
-                cnt = 1;
-            } else {
-                b.setBackgroundResource(R.drawable.o);
+                button.setBackgroundResource(R.drawable.x);
+                button.setEnabled(false);
+                if (button == top1) {
+                    fieldArray[0][0] = 'X';
+                }
+                if (button == top2) {
+                    fieldArray[0][1] = 'X';
+                }
+                if (button == top3) {
+                    fieldArray[0][2] = 'X';
+                }
+                if (button == mid1) {
+                    fieldArray[1][0] = 'X';
+                }
+                if (button == mid2) {
+                    fieldArray[1][1] = 'X';
+                }
+                if (button == mid3) {
+                    fieldArray[1][2] = 'X';
+                }
+                if (button == bot1) {
+                    fieldArray[2][0] = 'X';
+                }
+                if (button == bot2) {
+                    fieldArray[2][1] = 'X';
+                }
+                if (button == bot3) {
+                    fieldArray[2][2] = 'X';
+                }
                 cnt += 1;
+                checkifwon();
+            } else {
+                button.setBackgroundResource(R.drawable.o);
+                if (button == top1) {
+                    fieldArray[0][0] = 'O';
+                }
+                if (button == top2) {
+                    fieldArray[0][1] = 'O';
+                }
+                if (button == top3) {
+                    fieldArray[0][2] = 'O';
+                }
+                if (button == mid1) {
+                    fieldArray[1][0] = 'O';
+                }
+                if (button == mid2) {
+                    fieldArray[1][1] = 'O';
+                }
+                if (button == mid3) {
+                    fieldArray[1][2] = 'O';
+                }
+                if (button == bot1) {
+                    fieldArray[2][0] = 'O';
+                }
+                if (button == bot2) {
+                    fieldArray[2][1] = 'O';
+                }
+                if (button == bot3) {
+                    fieldArray[2][2] = 'O';
+                }
+                cnt += 1;
+                checkifwon();
             }
             return sign;
         } else {
             Toast.makeText(getApplicationContext(), "Spieler " + winner + "hat gewonnen", Toast.LENGTH_LONG).show();
         }
         return null;
+    }
+
+    private boolean checkifwon() {
+        //top
+        if (fieldArray[0][0] == 'X' && fieldArray[0][1] == 'X' && fieldArray[0][2] == 'X') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        if (fieldArray[0][0] == 'O' && fieldArray[0][1] == 'O' && fieldArray[0][2] == 'O') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        //mid
+        if (fieldArray[1][0] == 'X' && fieldArray[1][1] == 'X' && fieldArray[1][2] == 'X') {
+            gameWon(fieldArray[1][0]);
+            return true;
+        }
+        if (fieldArray[1][0] == 'O' && fieldArray[1][1] == 'O' && fieldArray[1][2] == 'O') {
+            gameWon(fieldArray[1][0]);
+            return true;
+        }
+        //bottom
+        if (fieldArray[2][0] == 'X' && fieldArray[2][1] == 'X' && fieldArray[2][2] == 'X') {
+            gameWon(fieldArray[2][0]);
+            return true;
+        }
+        if (fieldArray[2][0] == 'O' && fieldArray[2][1] == 'O' && fieldArray[2][2] == 'O') {
+            gameWon(fieldArray[2][0]);
+            return true;
+        }
+        //diagonal
+        if (fieldArray[0][0] == 'X' && fieldArray[1][1] == 'X' && fieldArray[2][2] == 'X') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        if (fieldArray[0][2] == 'X' && fieldArray[1][1] == 'X' && fieldArray[2][0] == 'X') {
+            gameWon(fieldArray[0][2]);
+            return true;
+        }
+        if (fieldArray[0][2] == 'O' && fieldArray[1][1] == 'O' && fieldArray[2][0] == 'O') {
+            gameWon(fieldArray[0][2]);
+            return true;
+        }
+        if (fieldArray[0][0] == 'O' && fieldArray[1][1] == 'O' && fieldArray[2][2] == 'O') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        //left
+        if (fieldArray[0][0] == 'X' && fieldArray[1][0] == 'X' && fieldArray[2][0] == 'X') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        if (fieldArray[0][0] == 'O' && fieldArray[1][0] == 'O' && fieldArray[2][0] == 'O') {
+            gameWon(fieldArray[0][0]);
+            return true;
+        }
+        //middown
+        if (fieldArray[0][1] == 'X' && fieldArray[1][1] == 'X' && fieldArray[2][1] == 'X') {
+            gameWon(fieldArray[0][1]);
+            return true;
+        }
+        if (fieldArray[0][1] == 'O' && fieldArray[1][1] == 'O' && fieldArray[2][1] == 'O') {
+            gameWon(fieldArray[0][1]);
+            return true;
+        }
+        //right
+        if (fieldArray[0][2] == 'X' && fieldArray[1][2] == 'X' && fieldArray[2][2] == 'X') {
+            gameWon(fieldArray[0][2]);
+            return true;
+        }
+        if (fieldArray[0][2] == 'O' && fieldArray[1][2] == 'O' && fieldArray[2][2] == 'O') {
+            gameWon(fieldArray[0][2]);
+            return true;
+        }
+        return false;
+    }
+
+    public void gameWon(Character character) {
+        String winner_name;
+        if (character == 'O') {
+
+            winner_name = playerObject1.getName();
+            playerObject1.setPunktezahl(playerObject1.getPunktezahl() + 1);
+            points_p1.setText(playerObject1.getPunktezahl() + "");
+
+        } else {
+            winner_name = playerObject2.getName();
+            playerObject2.setPunktezahl(playerObject2.getPunktezahl() + 1);
+            points_p2.setText(playerObject2.getPunktezahl() + "");
+        }
+        Toast.makeText(getApplicationContext(), winner_name + " hat gewonnen!", Toast.LENGTH_SHORT).show();
+        top1.setBackgroundResource(R.drawable.button_border);
+        top1.setEnabled(true);
+        top2.setBackgroundResource(R.drawable.button_border);
+        top2.setEnabled(true);
+        top3.setBackgroundResource(R.drawable.button_border);
+        top3.setEnabled(true);
+        mid1.setBackgroundResource(R.drawable.button_border);
+        mid1.setEnabled(true);
+        mid2.setBackgroundResource(R.drawable.button_border);
+        mid2.setEnabled(true);
+        mid3.setBackgroundResource(R.drawable.button_border);
+        mid3.setEnabled(true);
+        bot1.setBackgroundResource(R.drawable.button_border);
+        bot1.setEnabled(true);
+        bot2.setBackgroundResource(R.drawable.button_border);
+        bot2.setEnabled(true);
+        bot3.setBackgroundResource(R.drawable.button_border);
+        bot3.setEnabled(true);
+
+        for (int i = 0; i < fieldArray.length; i++) {
+            for (int o = 0; o < fieldArray.length; o++) {
+                fieldArray[i][o] = 'a';
+            }
+        }
     }
 }
