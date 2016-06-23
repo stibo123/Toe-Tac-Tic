cb5337c3140a699df26cafd40b5229ccfde444b6
diepes7 <olivero.dieplinger@gmail.com>
2016-06-23 11:34:53 +0200

    AlertDialog to delete Stats

--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/Preference_Activity.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/Preference_Activity.java
@@ -1,5 +1,7 @@
 package com.example.stefanbartos.toe_tac_tic;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -7,12 +9,14 @@ import android.os.Bundle;
 
 public class Preference_Activity extends PreferenceActivity{
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new myPreferenceFragment()).commit();
 }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class myPreferenceFragment extends PreferenceFragment
     {
         @Override
--- Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/Stats.java
+++ Toe-Tac-Tic/app/src/main/java/com/example/stefanbartos/toe_tac_tic/Stats.java
@@ -1,13 +1,21 @@
 package com.example.stefanbartos.toe_tac_tic;
 
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,57 +24,76 @@ public class Stats extends AppCompatActivity {
     ListView listView;
     ArrayList<String> al = new ArrayList<>();
     ArrayAdapter<String> adapter = null;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
         listView = (ListView) findViewById(R.id.listView);
+        initVariables();
         initListData();
         displayItems();
     }
 
-    private void initListData() {
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        SQLiteDatabase db = databaseHelper.getReadableDatabase();
-        Cursor cursor = db.query(Schemaklasse.TABLE_NAME, Schemaklasse.All_COLUMNS, null, null, null, null, null);
-        int Player1 = cursor.getColumnIndex(Schemaklasse.Player1);
-        int Player2 = cursor.getColumnIndex(Schemaklasse.Player2);
-        int player1score = cursor.getColumnIndex(Schemaklasse.player1score);
-        int player2score = cursor.getColumnIndex(Schemaklasse.player2score);
-        while (cursor.moveToNext()) {
-            al.add(cursor.getString(Player1) + ":" + cursor.getString(Player2) + "  \t " + cursor.getInt(player1score) + ":" + cursor.getInt(player2score));
+    private void initVariables(){
+        databaseHelper = new DatabaseHelper(this);
+        db = databaseHelper.getWritableDatabase();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                entrySelected(position);
+            }
+        });
+    }
+
+        {
+                    displayItems();
+                }
+            });
+
+            alert.show();
+
         }
-        cursor.close();
-        db.close();
-    }
 
-    private void displayItems() {
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
-        listView.setAdapter(adapter);
-    }
+                private void initListData() {
+                    Cursor cursor = db.query(Schemaklasse.TABLE_NAME, Schemaklasse.All_COLUMNS, null, null, null, null, null);
+                    int Player1 = cursor.getColumnIndex(Schemaklasse.Player1);
+                    int Player2 = cursor.getColumnIndex(Schemaklasse.Player2);
+                    int player1score = cursor.getColumnIndex(Schemaklasse.player1score);
+                    int player2score = cursor.getColumnIndex(Schemaklasse.player2score);
+                    while (cursor.moveToNext()) {
+                        al.add(cursor.getString(Player1) + ":" + cursor.getString(Player2) + "  \t " + cursor.getInt(player1score) + ":" + cursor.getInt(player2score));
+                    }
+                    cursor.close();
+                    db.close();
+                }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.deleteallstats:
-                deleteallStats();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+                private void displayItems() {
+                    adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+                    listView.setAdapter(adapter);
+                }
 
-    private void deleteallStats() {
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
-        db.delete(Schemaklasse.TABLE_NAME, null, null);
-        al.clear();
-        adapter.notifyDataSetChanged();
-    }
+                @Override
+                public boolean onOptionsItemSelected(MenuItem item) {
+                    int id = item.getItemId();
+                    switch (id) {
+                        case R.id.deleteallstats:
+                            deleteallStats();
+                    }
+                    return super.onOptionsItemSelected(item);
+                }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_stats, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-}
+                private void deleteallStats() {
+                    db.delete(Schemaklasse.TABLE_NAME, null, null);
+                    al.clear();
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public boolean onCreateOptionsMenu(Menu menu) {
+                    getMenuInflater().inflate(R.menu.menu_stats, menu);
+                    return super.onCreateOptionsMenu(menu);
+                }
+            }
